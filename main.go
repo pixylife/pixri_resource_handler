af4752b9475f1e208c79e4de82d4e2b78ae4a96d
Sahan Thinusha <sahanvijaya@gmail.com>
2020-04-21 02:46:09 +0530

    Added some color configs

--- main.go
+++ main.go
@@ -1,1 +1,14 @@
-package pixri_resource_handler
+package main
+
+import (
+	"fmt"
+	"github.com/lucasb-eyer/go-colorful"
+	"pixri_resource_handler/pkg"
+	_ "pixri_resource_handler/pkg"
+)
+
+func main(){
+color,_ := colorful.Hex("#9c64a6")
+	c := pkg.GetTextColor(color)
+	fmt.Println(c.Hex())
+}
--- pkg/color.go
+++ pkg/color.go
@@ -1,1 +1,21 @@
 package pkg
+
+import (
+	"github.com/lucasb-eyer/go-colorful"
+)
+
+func ColorBlend(c1 colorful.Color,c2 colorful.Color,variation int) colorful.Color{
+	blocks := 10
+	return c1.BlendRgb(c2, float64(variation)/float64(blocks-1))
+}
+
+func GetTextColor(c colorful.Color) colorful.Color{
+		if(c.B+c.G+c.B)>0.179{
+		 color,_ := colorful.Hex("#000000")
+		return color
+	}else {
+		color,_ := colorful.Hex("#FFFFFF")
+		return color
+	}
+
+}
